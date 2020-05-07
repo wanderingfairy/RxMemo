4fa0d5308d18405a3d7d4b515022995434aa5583
panda <gusauddml@gmail.com>
2020-05-08 05:53:07 +0900

    MemoList Scene

--- RxMemo/RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/RxMemo/ViewController/MemoListViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoListViewController: UIViewController, ViewModelBindableType {
   
-  
   var viewModel: MemoListViewModel!
   
+  @IBOutlet weak var listTableView: UITableView!
+  
+  @IBOutlet weak var addButton: UIBarButtonItem!
   
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +26,14 @@ class MemoListViewController: UIViewController, ViewModelBindableType {
     }
    
   func bindViewModel() {
+    viewModel.title
+      .drive(navigationItem.rx.title)
+      .disposed(by: rx.disposeBag)
     
+    viewModel.memoList
+      .bind(to: listTableView.rx.items(cellIdentifier: "cell")) { row, memo, cell in
+        cell.textLabel?.text = memo.content
+    }
+    .disposed(by: rx.disposeBag)
   }
 }
